fb8ea07527344d985118dd8d5040e6c6a82bd410
Bram <bram@fluet.dev>
2025-09-15 14:56:00 +0200

    cleaned up + base for react frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+      <h1>Welkom bij de BloemenVeiling</h1>
+    </div>
+  );
+};
+
+export default App;
