99528fc4adbf56687efe8365cf9dbc7b356807c5
Bram <bram@fluet.dev>
2025-10-29 10:03:34 +0100

    New navbar + font

--- frontend/src/theme.ts
+++ frontend/src/theme.ts
@@ -1,6 +1,9 @@
 import { createTheme } from "@mui/material/styles";
 
 export const theme = createTheme({
+  typography: {
+    fontFamily: `"Montserrat", "Helvetica", "Arial", sans-serif`,
+  },
   palette: {
     primary: {
       main: "#1f2225ff",
