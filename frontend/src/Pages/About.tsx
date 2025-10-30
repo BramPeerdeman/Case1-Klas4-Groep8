9366375549423fc2fa204530c7ffc95c6093df80
S3mCost3r <23094354@student.hhs.nl>
2025-10-30 11:49:10 +0100

    alt teksten toegevoegd

--- frontend/src/Pages/About.tsx
+++ frontend/src/Pages/About.tsx
@@ -5,11 +5,11 @@ export default function About() {
   <h2>Bloemen veilingen voor de toekomst</h2>
   <img
   src="https://np-royalfloraholland-production.s3-eu-west-1.amazonaws.com/_1366xAUTO_fit_center-center_80_none/royal-floraholland-eelde.jpg"
-  alt="FloraHolland online veiling"
+  alt="FloraHolland gebouw"
   style={{ width: "100%", borderRadius: "12px" }}
 />
-  <p>"Wij willen hier bij bloemenveiling.nl de toekomst aanbieden voor alle bloemisten en teelers!"</p>
-  <p>"Tegenwoordig heeft iedereen een mobiel of computer en naast dat het slecht is voor het milieu wil niet iedereen reizen naar aalsmeer(FloraHolland) om daar biedingen te doen. Daarom hebben wij Bloemenveiling.nl ontwikkeld"</p>
+  <p>Wij willen hier bij bloemenveiling.nl de toekomst aanbieden voor alle bloemisten en teelers!</p>
+  <p>Tegenwoordig heeft iedereen een mobiel of computer en naast dat het slecht is voor het milieu wil niet iedereen reizen naar aalsmeer(FloraHolland) om daar biedingen te doen. Daarom hebben wij Bloemenveiling.nl ontwikkeld</p>
   </>
   );
 }
