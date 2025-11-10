5356bf6989a470ef694eec40d6ff4be19104abf2
drdoom <22122699@student.hhs.nl>
2025-11-10 10:57:03 +0100

    cors toegevoegd hope it works

--- backend/Program.cs
+++ backend/Program.cs
@@ -12,6 +12,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddCors(o =>
+{
+    o.AddDefaultPolicy(p => p
+        .WithOrigins("http://localhost:5173", "https://localhost:5173")
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .AllowCredentials());
+        });
+
+
 // --- Controllers + Routing ---
 builder.Services.AddControllers()
     .AddJsonOptions(options =>
@@ -69,6 +79,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseRouting();
+app.UseCors();
 app.MapHub<AuctionHub>("/AuctionHub");
 app.MapControllers();
 
