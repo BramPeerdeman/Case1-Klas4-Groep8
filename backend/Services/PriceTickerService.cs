766b5a26659cbd75bc673b7381c9d51209cb1b8a
drdoom <22122699@student.hhs.nl>
2025-11-26 11:29:54 +0100

    start service

---
+++ backend/Services/PriceTickerService.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Hosting;
+using backend.Hubs;
+
+namespace backend.Services
+{
+     /*public class PriceTickerService : BackgroundService
+    {
+        private readonly IHubContext<AuctionHub> _hub;
+        public PriceTickerService(IHubContext<AuctionHub> hub)
+        {
+            _hub = hub;
+        }
+
+    } */
+}
