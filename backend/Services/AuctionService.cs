ad8c921d629964cec04226a336d6158ca2eb69ff
drdoom <22122699@student.hhs.nl>
2025-12-08 11:06:29 +0100

    commit update to auction service

---
+++ backend/Services/AuctionService.cs
@@ -0,0 +1,34 @@
+﻿using backend.Data;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace backend.Services
+{
+    public class AuctionService
+    {
+        private readonly AppDbContext _db;
+
+        public AuctionService(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task MoveNewAuctionableProducts(CancellationToken ct = default)
+        {
+            var NewlyAuctionableProducts = await _db.Producten
+                .Where(p=>p.StartPrijs != null && !p.IsAuctionable)
+                .ToListAsync(ct);
+
+            if (NewlyAuctionableProducts.Count != 0)
+                return;
+
+            foreach (var p in NewlyAuctionableProducts)
+            {
+                p.IsAuctionable = true;
+            }
+
+            await _db.SaveChangesAsync(ct);
+        }
+    }
+
+}
