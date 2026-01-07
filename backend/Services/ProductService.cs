663bdbb2591ec5620ddbcc4fec381ddc32b36376
AndroidTheHuman <24109320@student.hhs.nl>
2026-01-07 14:06:17 +0100

    Velingmeesterpagina update 3

--- backend/Services/ProductService.cs
+++ backend/Services/ProductService.cs
@@ -3,9 +3,9 @@ using backend.interfaces;
 using backend.Models;
 using Microsoft.EntityFrameworkCore;
 
-namespace backend.Services  
+namespace backend.Services
 {
-    public class ProductService:IProductService
+    public class ProductService : IProductService
     {
         private readonly AppDbContext _context;
         public ProductService(AppDbContext context)
@@ -13,11 +13,15 @@ namespace backend.Services
             _context = context;
         }
 
-        public async Task <List<Product>> GetAuctionableProductsAsync()
+        public async Task<List<Product>> GetAuctionableProductsAsync()
         {
+            var today = DateTime.Today;
+
             return await _context.Producten
                 .AsNoTracking()
-                .Where(p => p.StartPrijs != null)
+                .Where(p => p.StartPrijs != null &&
+                            p.BeginDatum.HasValue &&
+                            p.BeginDatum.Value.Date == today)
                 .ToListAsync();
         }
     }
